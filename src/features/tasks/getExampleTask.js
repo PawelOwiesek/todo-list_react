11f1f695d3a92be5e05d7269b5f6d21505611002
Pawel Owiesek <Owiesek.p@gmail.com>
2023-06-16 14:35:04 +0200

    removed console.log and removed executing the func

--- src/features/tasks/getExampleTask.js
+++ src/features/tasks/getExampleTask.js
@@ -2,12 +2,11 @@ import axios from "axios";
 
 const fetchData = "/todo-list_react/exampleTasks.json";
 
-export const getExampleTasks = (async () => {
+export const getExampleTasks = async () => {
   try {
     const response = await axios.get(fetchData);
-    console.log(response.data);
-    return await response.data;
+    return response.data;
   } catch (error) {
     return new Error(error).statusText;
   }
-})();
+};
