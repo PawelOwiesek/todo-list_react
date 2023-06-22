005708afff5c49acf6d15868a34b2c0f872d96a7
Pawel Owiesek <Owiesek.p@gmail.com>
2023-06-22 17:50:29 +0200

    created router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,29 @@
+import { HashRouter, Link, Switch, Route } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
-const App = () => <Tasks />;
+import { Author } from "./features/author/Author";
+const App = () => (
+  <HashRouter>
+    <div style={{ display: "flex" }}>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/author">About author</Link>
+          </li>
+          <li>
+            <Link to="/tasks">Tasks</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/author">
+            <Author />
+          </Route>
+          <Route exactpath="/tasks">
+            <Tasks />
+          </Route>
+        </Switch>
+      </nav>
+    </div>
+  </HashRouter>
+);
 export default App;
