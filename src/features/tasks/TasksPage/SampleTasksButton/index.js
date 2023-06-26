5e7cd14c187b2ef61e50a7fd745904ce8259dc69
Pawel Owiesek <Owiesek.p@gmail.com>
2023-06-26 18:56:37 +0200

    removed styles and use effect for paragraph

--- src/features/tasks/TasksPage/SampleTasksButton/index.js
+++ src/features/tasks/TasksPage/SampleTasksButton/index.js
@@ -1,34 +1,17 @@
 import { StyledButton } from "../Buttons/StyledButtons";
-import { fetchExampleTasks, fetchingStatus } from "../../tasksSlice";
+import { fetchExampleTasks, fetchingisLoading } from "../../tasksSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { LoadingParagraph } from "./styled";
-import { useState, useEffect } from "react";
 
 const SampleTaskButton = () => {
-  const [red, setRed] = useState(false);
   const dispatch = useDispatch();
-  const loading = useSelector(fetchingStatus);
-
-  useEffect(() => {
-    const intervalID = setInterval(() => {
-      setRed((red) => !red);
-    }, 500);
-
-    return () => {
-      clearInterval(intervalID);
-    };
-  }, []);
+  const loading = useSelector(fetchingisLoading);
 
   return (
     <StyledButton
       onClick={() => dispatch(fetchExampleTasks())}
       disabled={loading}
     >
-      {loading ? (
-        <LoadingParagraph red={red}>Loading...</LoadingParagraph>
-      ) : (
-        <LoadingParagraph>Download sample tasks.</LoadingParagraph>
-      )}
+      {loading ? "Loading..." : "Download sample tasks"}
     </StyledButton>
   );
 };
--- src/features/tasks/TasksPage/SampleTasksButton/styled.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const LoadingParagraph = styled.p`
-  color: ${({ red }) => (red ? "red" : `${({ theme }) => theme.colors.teal}`)};
-`;
