071bd116eea3a6ca74a1a192082155189d3003fd
Pawel Owiesek <Owiesek.p@gmail.com>
2023-06-26 18:57:54 +0200

    removed eslint comment line

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-anonymous-default-export */
 import Container from "../../common/Container";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
